4311b87246328cf4fbd6cc3411713094033c364a
Vincent <eohndk@gmail.com>
2025-08-12 21:33:51 -0700

    fixed shadcn components init

--- client/src/pages/popup/Popup.tsx
+++ client/src/pages/popup/Popup.tsx
@@ -1,3 +1,32 @@
+import { Button } from "@src/components/ui/button";
+import { Input } from "@src/components/ui/input";
+import { Label } from "@src/components/ui/label";
+import { useState } from "react";
+
 export default function Popup() {
-  return "hello";
+  const [server, setServer] = useState("");
+
+  function handleSetServer(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    setServer(form["server_input"].value);
+  }
+
+  return (
+    <>
+      <Label>Server:</Label>
+      <form
+        className="flex flex-row"
+        onSubmit={(e) => {
+          handleSetServer(e);
+        }}>
+        <Input className="flex-1" name="server_input" />
+        <Button>Submit</Button>
+      </form>
+      <Label>
+        Server set as:<span>{server}</span>
+      </Label>
+    </>
+  );
 }
